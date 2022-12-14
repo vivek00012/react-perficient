277ae9b94983fc81da15ed1c46407c3b0cc97011
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2022-12-14 16:32:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [toDoList, setList] = useState([]);
+  const [inputValue, setValue] = useState('');
+
+  const setInputValue = (e) => {
+    setValue(e.target.value);
+  };
+  const addToDo = () => {
+    setList((previousState) => {
+      return [...previousState, ...[inputValue]];
+    });
+
+    setValue(() => '');
+  };
+
+  const removeToDo = (value) => {
+    console.log(value);
+    setList((previousState) => {
+      return previousState.filter((val,index) => index !=value);
+    });
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ul>
+        {toDoList?.map((list, index) => {
+          return (
+            <div key={index}>
+              <li>{list}</li>
+              <button onClick={() => removeToDo(index)}>Remove</button>
+            </div>
+          );
+        })}
+      </ul>
+      <div>
+        <input type="text" value={inputValue} onChange={setInputValue} /> &nbsp;
+        <button onClick={addToDo}>Add</button>
+        &nbsp;
+      </div>
     </div>
   );
 }
